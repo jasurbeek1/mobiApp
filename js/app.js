63732c800179c1e82d1c2f175f7ef826525aba97
jasur228 <abdurahmonovjasur541@gmail.com>
2023-07-12 07:07:25 +0300

    mobil-app alarm idet

---
+++ js/app.js
@@ -0,0 +1,231 @@
+const calculatorImg = document.querySelector(`#calculatorBtn`)
+const calculator = document.querySelector(`.colculator`)
+const backBtn = document.querySelector(`#backBtn`)
+const footerApp = document.querySelector(`.box-app_footer`)
+const numbers = document.querySelector(`.numbers`)
+const result = document.querySelector(`.result input`)
+const timer =  document.querySelector(`.timer`)
+const dataHours = document.querySelector(`.data-hours`)
+const dataDay = document.querySelector(`.data-day`);
+const boxApp = document.querySelector(`.box-app`)
+
+// ! data timer
+
+function getDateTime() {
+
+    const now = new Date()
+    const date = now.getDate()<10 ? "0" +now.getDate() : now.getDate()
+    const month = now.getMonth()<10 ?`0` + (now.getMonth()+1):now.getMonth
+    const year = now.getFullYear()
+    
+
+    const hour = now.getHours()<10 ? "0" + now.getHours() : now.getHours()
+    const minute = now.getMinutes()<10 ? "0" + now.getMinutes() : now.getMinutes()
+    const monhts = [
+        `January`,
+        `February`,
+        `March`,
+        `April`,
+        `May`,
+        `June`,
+        `July`,
+        `August`,
+        `September`,
+        `October`,
+        `November`,
+        `December`
+    ]
+    const month_title = now.getMonth()
+    dataDay.textContent = `${date} ${monhts[month_title]},${year}`
+    dataHours.textContent= `${hour}:${minute}`
+    return `${hour}:${minute}, ${date}.${month}.${year}`
+}
+
+getDateTime()
+
+
+
+// ! calculator style start
+calculator.style.display = `none`
+backBtn.style.display =`none`
+footerApp.style.display=`flex`
+
+calculatorImg.addEventListener(`click`, (e) => {
+    e.preventDefault()
+    if(calculator.style.display = `none`){
+        calculator.style.display = `block`
+        boxApp.style.display = `none`
+        timer.style.display= `none`
+        backBtn.style.display =`block`
+        footerApp.style.display=`none`        
+    }
+})
+backBtn.addEventListener(`click`, (e) => {
+    calculator.style.display = `none`
+    boxApp.style.display = `flex`
+    timer.style.display= `flex`
+})
+
+
+
+
+
+
+
+//Initial References
+let timerRef = document.querySelector(".timer-display");
+const hourInput = document.getElementById("hourInput");
+const minuteInput = document.getElementById("minuteInput");
+const activeAlarms = document.querySelector(".activeAlarms");
+const setAlarm = document.getElementById("set");
+let alarmsArray = [];
+let alarmSound = new Audio("./alarm.mp3");
+
+let initialHour = 0,
+  initialMinute = 0,
+  alarmIndex = 0;
+
+//Append zeroes for single digit
+const appendZero = (value) => (value < 10 ? "0" + value : value);
+
+//Search for value in object
+const searchObject = (parameter, value) => {
+  let alarmObject,
+    objIndex,
+    exists = false;
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm[parameter] == value) {
+      exists = true;
+      alarmObject = alarm;
+      objIndex = index;
+      return false;
+    }
+  });
+  return [exists, alarmObject, objIndex];
+};
+
+//Display Time
+function displayTimer() {
+  let date = new Date();
+  let [hours, minutes, seconds] = [
+    appendZero(date.getHours()),
+    appendZero(date.getMinutes()),
+    appendZero(date.getSeconds()),
+  ];
+
+  //Display time
+  timerRef.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+  //Alarm
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm.isActive) {
+      if (`${alarm.alarmHour}:${alarm.alarmMinute}` === `${hours}:${minutes}`) {
+        alarmSound.play();
+        alarmSound.loop = true;
+      }
+    }
+  });
+}
+
+const inputCheck = (inputValue) => {
+  inputValue = parseInt(inputValue);
+  if (inputValue < 10) {
+    inputValue = appendZero(inputValue);
+  }
+  return inputValue;
+};
+
+hourInput.addEventListener("input", () => {
+  hourInput.value = inputCheck(hourInput.value);
+});
+
+minuteInput.addEventListener("input", () => {
+  minuteInput.value = inputCheck(minuteInput.value);
+});
+
+//Create alarm div
+
+const createAlarm = (alarmObj) => {
+  //Keys from object
+  const { id, alarmHour, alarmMinute } = alarmObj;
+  //Alarm div
+  let alarmDiv = document.createElement("div");
+  alarmDiv.classList.add("alarm");
+  alarmDiv.setAttribute("data-id", id);
+  alarmDiv.innerHTML = `<span>${alarmHour}: ${alarmMinute}</span>`;
+
+  //checkbox
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      startAlarm(e);
+    } else {
+      stopAlarm(e);
+    }
+  });
+  alarmDiv.appendChild(checkbox);
+  //Delete button
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteButton.classList.add("deleteButton");
+  deleteButton.addEventListener("click", (e) => deleteAlarm(e));
+  alarmDiv.appendChild(deleteButton);
+  activeAlarms.appendChild(alarmDiv);
+};
+
+//Set Alarm
+setAlarm.addEventListener("click", () => {
+  alarmIndex += 1;
+
+  //alarmObject
+  let alarmObj = {};
+  alarmObj.id = `${alarmIndex}_${hourInput.value}_${minuteInput.value}`;
+  alarmObj.alarmHour = hourInput.value;
+  alarmObj.alarmMinute = minuteInput.value;
+  alarmObj.isActive = false;
+  console.log(alarmObj);
+  alarmsArray.push(alarmObj);
+  createAlarm(alarmObj);
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+});
+
+//Start Alarm
+const startAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = true;
+  }
+};
+
+//Stop alarm
+const stopAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = false;
+    alarmSound.pause();
+  }
+};
+
+//delete alarm
+const deleteAlarm = (e) => {
+  let searchId = e.target.parentElement.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    e.target.parentElement.parentElement.remove();
+    alarmsArray.splice(index, 1);
+  }
+};
+
+window.onload = () => {
+  setInterval(displayTimer);
+  initialHour = 0;
+  initialMinute = 0;
+  alarmIndex = 0;
+  alarmsArray = [];
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+};
